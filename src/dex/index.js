60b205b7391b4bd0a2160e4a88bb1cde38f95aac
sshubhamagg <shubhamsgl@invbrain.in>
2022-09-28 19:16:56 +0530

    Added support for pancakeswap and quickswap

--- src/dex/index.js
+++ src/dex/index.js
@@ -1,6 +1,7 @@
 const oneInch = require('@getsafle/1inch-controller');
 const uniswap = require('@getsafle/uniswap-controller');
-
+const pancakeswap = require('@getsafle/pancakeswap-controller');
+const quickswap = require('@getsafle/uniswap-controller');
 const supportedDex = [ 'oneInch', 'uniswap' ];
 
-module.exports = {supportedDex, uniswap, oneInch};
+module.exports = {supportedDex, uniswap, oneInch, pancakeswap, quickswap};
